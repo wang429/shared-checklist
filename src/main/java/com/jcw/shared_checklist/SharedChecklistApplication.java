f2aa4bb04768cd7bf7f0941716f1f3d2c54faeb5
James Wang <007jcw@gmail.com>
2025-06-22 14:44:38 -0400

    spring initializr

---
+++ src/main/java/com/jcw/shared_checklist/SharedChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.jcw.shared_checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedChecklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcw/shared_checklist/SharedChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcw.shared_checklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
